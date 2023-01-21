2d4930498247b3e06fc071971f8c182987992fbc
Monoget <monoget2@gmail.com>
2023-01-21 19:36:30 +0600

    update all

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -209,8 +209,6 @@ jQuery(function ($) {
 	});
 
 	// Switch Btn
-	$('body').append("<div class='switch-box'><label id='switch' class='switch'><input type='checkbox' onchange='toggleTheme()' id='slider'><span class='slider round'></span></label></div>");
-
 }(jQuery));
 
 // function to set a given theme/color-scheme
